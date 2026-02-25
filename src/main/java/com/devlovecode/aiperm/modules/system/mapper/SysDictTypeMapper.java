1226db81716fa0953440d1fa6686a5dde5024604
shichenyang <gyv12345@163.com>
2026-02-25 10:32:39 +0800

    refactor(dict): 删除旧的 Mapper、Service接口、Request DTO 文件

--- src/main/java/com/devlovecode/aiperm/modules/system/mapper/SysDictTypeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.devlovecode.aiperm.modules.system.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.devlovecode.aiperm.modules.system.entity.SysDictType;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysDictTypeMapper extends BaseMapper<SysDictType> {
-}
