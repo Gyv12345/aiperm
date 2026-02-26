bd6fb4490594ae6dbd4635073181ec27b3f27f46
shichenyang <gyv12345@163.com>
2026-02-26 16:03:49 +0800

    feat(data-scope): add DataScopeEnum

---
+++ backend/src/main/java/com/devlovecode/aiperm/common/enums/DataScopeEnum.java
@@ -0,0 +1,34 @@
+package com.devlovecode.aiperm.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 数据权限范围枚举
+ *
+ * @author DevLoveCode
+ */
+@Getter
+@AllArgsConstructor
+public enum DataScopeEnum {
+
+    ALL(1, "全部数据"),
+    DEPT(2, "本部门数据"),
+    DEPT_AND_CHILD(3, "本部门及下级部门数据"),
+    SELF(4, "仅本人数据");
+
+    private final Integer code;
+    private final String desc;
+
+    public static DataScopeEnum of(Integer code) {
+        if (code == null) {
+            return ALL;
+        }
+        for (DataScopeEnum value : values()) {
+            if (value.getCode().equals(code)) {
+                return value;
+            }
+        }
+        return ALL;
+    }
+}
